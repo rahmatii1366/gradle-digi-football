736d4bcba8e74b7bda28f5049ccf680d6355ba30
m.rahmati <rahmatii1366@gmail.com>
2019-06-18 19:12:47 +0430

    resolve test

--- test/src/main/java/ir/piana/dev/test/business/service/TestApiImpl.java
+++ test/src/main/java/ir/piana/dev/test/business/service/TestApiImpl.java
@@ -4,11 +4,13 @@ import io.reactivex.Single;
 import ir.piana.dev.test.server.api.dto.PostDto;
 import ir.piana.dev.test.server.api.dto.ResponseDto;
 import ir.piana.dev.test.server.api.service.TestApi;
+import org.springframework.stereotype.Component;
 
 /**
  * @author Mohamad Rahmati (rahmatii1366@gmail.com)
  * Date: 6/18/2019 2:21 PM
  **/
+@Component
 public class TestApiImpl implements TestApi {
     @Override
     public Single<ResponseDto> postTest(PostDto argument) {
